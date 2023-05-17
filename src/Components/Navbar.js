7a63bc4a1eee02569aee7b2c9d8a7779a1892a9e
kaur-rajdeep <kaurd3535@gmail.com>
2023-05-17 22:04:47 +0530

    made the navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react"
+export default function Navbar(){
+    return (
+        <nav>
+            <img src="https://a16z.com/wp-content/uploads/2016/09/logo_airbnb2.png" width="100px"/>
+        </nav>
+    )
+}
