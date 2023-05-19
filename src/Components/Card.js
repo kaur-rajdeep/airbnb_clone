bd9409470ad7b18e5c5f652b8ee8ea4a743a51aa
kaur-rajdeep <kaurd3535@gmail.com>
2023-05-19 21:49:23 +0530

    hard coded the card component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,6 +2,15 @@ import React from "react"
 
 export default function Card(){
     return(
-        <h1>Card</h1>
+        <div className="card">
+            <img src="../images/card_image.png" className="card--image"/>
+            <div className = "card--stats"/>
+                <img src="../images/star.png" alt="small star" className="card--star"/>
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
+                <p>Life Lessons with Katie Zaferes</p>
+                <p><span className="bold">From $136 </span> / person</p>
+        </div>
     )
 }
