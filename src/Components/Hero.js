c11a265de1dad52bdb41bc84361941dd04352791
kaur-rajdeep <kaurd3535@gmail.com>
2023-05-19 10:34:57 +0530

    fixing warning

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,8 +1,8 @@
 import React from "react";
 export default function Hero(){
     return (
-        <section class="hero">
-            <img src="..images/photo-grid.png" className="hero--photo"/>
+        <section className="hero">
+            <img src="images/photo-grid.png" alt="photogrid" className="hero--photo"/>
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home. </p>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react"
 export default function Navbar(){
     return (
         <nav>
-            <img src="https://a16z.com/wp-content/uploads/2016/09/logo_airbnb2.png" class="nav--logo"/>
+            <img src="https://a16z.com/wp-content/uploads/2016/09/logo_airbnb2.png" className="nav--logo" alt="logo"/>
         </nav>
     )
 }
